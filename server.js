5fc78ec6f867690ce0d8a438d9fdab6b9fe11386
mofath <mohamed.elgaedi@gmail.com>
2021-02-03 23:47:41 +0200

    Playing with cluster module

--- server.js
+++ server.js
@@ -1,11 +1,26 @@
 const express = require("express");
+const cluster = require("cluster");
+const numCPUs = require("os").cpus().length;
 const fibObj = require("./math-logic/fibonacci-series");
 
-const app = express();
+if (cluster.isMaster) {
+  console.log("Number of cores: " + numCPUs);
 
-app.get("/", (req, res) => {
-  let number = fibObj.calculateFibonacciValue(parseInt(req.query.num));
-  return res.send(`<h1>${number}</h1>`);
-});
+  //Fork the workers, one per CPU core
+  for (let i = 0; i < numCPUs; i++) cluster.fork();
 
-app.listen(3000, () => console.log("express is running on port 3000"));
+  cluster.on("exit", (deadWorker, code, signal) => {
+    const worker = cluster.fork();
+    console.log(`worker ${deadWorker.process.pid} died.`);
+    console.log(`worker ${worker.process.pid} born.`);
+  });
+} else {
+  const app = express();
+  app.get("/", (req, res) => {
+    console.log(`Worker process ID - ${cluster.worker.process.pid} has accepted the request`);
+    let number = fibObj.calculateFibonacciValue(parseInt(req.query.num));
+    return res.send(`<h1>${number}</h1>`);
+  });
+
+  app.listen(3000, () => console.log(`express app is running on port 300 and by worker ${cluster.worker.process.pid} `));
+}
